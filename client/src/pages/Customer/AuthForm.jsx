9e48f824fa45599380d92bf5159af821f1e5d79e
vinay2005kumar <buttalavinay@gmail.com>
2025-03-19 21:55:26 +0530

    google

--- client/src/pages/Customer/AuthForm.jsx
+++ client/src/pages/Customer/AuthForm.jsx
@@ -33,7 +33,7 @@ const LoginForm = () => {
   const navigate = useNavigate();
   const { login } = useAuth();
   const passwordInputRef = useRef(null);
-  const url = 'https://palmyra-fruit.onrender.com/api/user';
+    const url = 'https://palmyra-fruit.onrender.com/api/user';
   // const url = "http://localhost:4000/api/user";// Backend API URL
 
   // Handle login form submission
